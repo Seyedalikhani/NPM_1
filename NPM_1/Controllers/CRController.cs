a724939b61c7740c6df043b7e96838d64bfd039b
ahmad_950_61@yahoo.com <ahmad_950_61@yahoo.com>
2025-08-09 10:21:48 +0330

    Spring

--- NPM_1/Controllers/CRController.cs
+++ NPM_1/Controllers/CRController.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data;
-using System.Data.SqlClient;
-using Excel = Microsoft.Office.Interop.Excel;
-using ClosedXML.Excel;
-using Newtonsoft.Json;
-using System.Web.Services;
-using System.Configuration;
-using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace NPM_1.Controllers
 {
@@ -23,13 +14,12 @@ namespace NPM_1.Controllers
             return View();
         }
 
-
         public ActionResult CR_KPI()
         {
 
-
            return View();
         }
+
 
     }
 }
---
+++ NPM_1/Controllers/Province.cs
@@ -0,0 +1,7 @@
+﻿namespace NPM_1.Controllers
+{
+    internal class Province
+    {
+        public string ProvinceName { get; internal set; }
+    }
+}
